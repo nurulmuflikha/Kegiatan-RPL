2f92a6dc667ca80d38bdd2c8983c965a832ffd66
unknown <nurulmuflikhaalka@gmail.com>
2021-12-24 12:09:46 +0700

    coba

---
+++ day1.py
@@ -0,0 +1,14 @@
+#day 1
+#RPL
+
+#membuat variabel beserta isinya
+nama = "NURUL MUFLIKHA"         #type data string
+umur = 19                       #type data integer
+tinggi = 153.5                  #type data float
+single = True                   #type data bollean
+
+#mencetak isi data variabel
+print ("nama = " , nama)
+print ("umur = " , umur)
+print ("tinggi = " , tinggi)
+print ("single = " , single)
---
+++ day2.py
@@ -0,0 +1,20 @@
+#day2
+#RPL
+
+#memahami penggunaan kondisi if elif
+
+hari_ini = "Rabu"
+if hari_ini == "senin":
+    print ("saya akan kuliah")
+elif hari_ini == "selasa":
+    print ("saya akan kuliah")
+elif hari_ini == "rabu":
+    print ("saya akan libur")
+elif hari_ini == "kamis":
+    print ("saya akan kuliah")
+elif hari_ini == "jumat":
+    print ("saya akan kuliah")
+elif hari_ini == "sabtu":
+    print ("saya akan libur")
+
+    
