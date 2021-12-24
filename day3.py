0ac027ed3c57a2274d56f87268779e24de9337b4
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-24 12:28:15 +0700

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,13 @@
+#day3
+#RPL
+
+#memahami percabangan if elif else
+
+cek = int (input ("cek bilangan = "))
+
+if cek == 30 :
+    print ("bilangan bulat")
+elif cek == 40 :
+    print ("bilangan bulat")
+else:
+    print ("bilangan negatif")
