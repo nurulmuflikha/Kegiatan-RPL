d1091bbef3c1e9d44fd8fb2728fff12d2cd49d5b
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-24 12:32:43 +0700

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,11 @@
+#day4
+#RPL
+
+#memahami perulangan for dengan fungsi range ()
+
+for a in range (10,30):
+    if a % 3 == 0 :
+        if a % 2 == 0 :
+            print (a)
+
+#menentukan hasil dari angka yang habis dibagi 3 dan habis dibagi 2
