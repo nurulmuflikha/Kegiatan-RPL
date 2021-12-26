f55225f0f831a0f2ab6aca952bd480870bc0850b
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-26 13:35:51 +0700

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,11 @@
+#day 6
+#RPL
+#memahami percabangan untuk membuat logika program
+lulus = input ("apakah kamu lulus : ")
+if lulus == "tidak":
+    print ("kamu harus ikut pengulangan")
+elif lulus == "iya":
+    print ("kamu sudah tuntas")
+else:
+    print ("kamu tidak ikut ujian")
+    
