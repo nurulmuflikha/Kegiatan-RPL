8ec102609da2e8e0b749e44128134541fd783672
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-26 20:47:34 +0700

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,12 @@
+#day15
+#RPL
+#memunculkan bilangan genap dan bilangan ganjil menngunakan percabangan
+
+ 
+for a in range (1,20):
+    if a % 2 == 0:
+        print ("ini adalah bilangan genap : " , a)
+    elif a % 2 != 0 :
+        print ("ini adalah bilangan ganjil : " , a)
+    
+    
