0ca91cb39fee643a55bb05f417af6f44fa9d1500
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-26 14:02:21 +0700

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,9 @@
+#day 8
+#RPL
+#belajar percabangan if else
+
+nama = input ("masukkan nama = ")
+if nama == "Nurul Muflikha" :
+    print ("ini adalah nama asli")
+else:
+    print ("bukan nama saya")
