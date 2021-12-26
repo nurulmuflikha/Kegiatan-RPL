fa54363bd4cadf2fb510b9d98664ed6075d9abdc
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-26 13:48:41 +0700

    Add files via upload

---
+++ day 7.py
@@ -0,0 +1,12 @@
+#day 7
+#RPL
+#mengecek type data
+
+nama = "nurul muflikha"
+print (nama , "merupakan type" , type (nama))
+umur = 19
+print (umur , "merupakan type" , type (umur))
+tinggi = 153.5
+print (tinggi , "merupakan type" , type (tinggi))
+single = True
+print (single , "merupakan type" , type (single))
