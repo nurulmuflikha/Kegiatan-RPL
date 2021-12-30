40d3e6506dd38aaa9e09606510b6e933db4cd4e5
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2021-12-30 20:42:18 +0700

    Add files via upload

---
+++ mikhaday19.py
@@ -0,0 +1,13 @@
+#day19
+#RPL
+#belajar tipe data tuple
+
+tuple_buah = ('Pisang', 'Nanas', 'Melon', 'Durian')
+
+print(tuple_buah[0:1])
+print(tuple_buah[0:2])
+print(tuple_buah[1:3])
+print(tuple_buah[0:-1])
+print(tuple_buah[-1:-3])
+print(tuple_buah[-1:3])
+print(tuple_buah[-3:-1])
