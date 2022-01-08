c2b661037489bdc810ad4f1f87393f60ed7cd0b4
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-08 21:41:44 +0800

    Add files via upload

---
+++ day28.py
@@ -0,0 +1,10 @@
+#belajar tuple
+tuple_buah = ('Pisang', 'Nanas', 'Melon', 'Durian')
+
+print(tuple_buah[0:1])
+print(tuple_buah[0:2])
+print(tuple_buah[1:3])
+print(tuple_buah[0:-1])
+print(tuple_buah[-1:-3])
+print(tuple_buah[-1:3])
+print(tuple_buah[-3:-1])
