9eeed911ba2b03ffd22843c068834cfc6e6c00c6
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-10 15:16:55 +0800

    Add files via upload

---
+++ day30.py
@@ -0,0 +1,39 @@
+#day30
+#RPL
+
+print ("#belajar fungsi list dengan memakai conteks append()")
+#menambahkan item didalam list menggunakan fungsi append()
+
+campuran = ["Mikha" , 19 , True]
+campuran.append(152.5)
+campuran.append("NURUL MUFLIKHA")
+
+print (campuran)
+
+
+print ("#belajar fungsi list dengan memakai conteks insert()")
+#menambah anggota list di dalam indek tertentu menggunakan fungsi insert()
+
+campuran = ["Mikha" , 19 , True]
+campuran.insert(19 , "Nurul Muflikha")
+
+print (campuran)
+
+
+print ("#belajar fungsi list dengan memakai conteks remove()")
+#menghapus item list
+
+campuran = ["Mikha" , 19 , True]
+campuran.remove(19)
+
+print (campuran)
+
+print ("#belajar fungsi list dengan memakai conteks len()")
+#menghitung panjang isi dari list
+
+campuran = ["Mikha" , 19 , True]
+
+print (len(campuran))
+
+
+
