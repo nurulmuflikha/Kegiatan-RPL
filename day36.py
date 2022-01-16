9510e283025d764f863d7d70def4b82dea417a46
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-16 22:21:11 +0800

    Add files via upload

---
+++ day36.py
@@ -0,0 +1,13 @@
+#Belajar tambah data
+list_a = [1]
+set_a = {1, 39}
+
+print('[Sebelum]')
+print(f'  list\t: {list_a}')
+print(f'  set\t: {set_a}')
+
+list_a.append(10)
+list_a.append(50)
+print('[Sesudah]')
+print(f'  list\t: {list_a}')
+print(f'  set\t: {set_a}')
