436750357c0b5b7ac94a3fbaa41b47a89220d900
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-19 19:29:39 +0800

    Add files via upload

---
+++ IPK DAY39.py
@@ -0,0 +1,14 @@
+ipk = input("masukkan grade IPK = ")
+
+if ipk == "A":
+    print("sempurna")
+elif ipk == "B":
+    print("bagus")
+elif ipk == "C":
+    print("cukup")
+elif ipk == "D":
+    print("kurang")
+elif ipk == "E":
+    print("parah")
+else:
+    print("grade IPK hanya ada A/B/C/D/E!")
