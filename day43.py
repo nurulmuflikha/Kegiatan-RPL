3d2e2458248f59d7a99b3d9e3622667bb95382a3
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-23 20:58:39 +0800

    Add files via upload

---
+++ day43.py
@@ -0,0 +1,5 @@
+# Menginput Alas dan Tinggi Segitiga
+alas = float(input('Tulis Alas Segitiga: '))
+tinggi = float(input('Tulis Tinggi Segitiga: '))
+luas = (alas * tinggi) / 2
+print('Luas Segitiga adalah %0.2f' %luas)
