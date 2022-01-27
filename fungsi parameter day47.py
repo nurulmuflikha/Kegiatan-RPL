2aaafeb80a91344af143bc65a752b70227327454
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-01-27 21:25:11 +0800

    Add files via upload

---
+++ fungsi parameter day47.py
@@ -0,0 +1,18 @@
+#belajar fungsi
+#parameter
+
+#tanpa paramater
+def panggil():
+	print ("hallo mikha")
+	print ("hallo chaa")
+	print ("hallo merr")
+	print ("hallo nay")
+panggil()
+
+#dengan parameter
+def panggil(nama):
+	print ("hallo" + nama)
+panggil("mikha")
+panggil ("nayy")
+panggil("merr")
+
