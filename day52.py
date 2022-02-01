8b3a72227edb6d2dbd45b0c91585c2e983de6af2
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-01 20:56:12 +0800

    Add files via upload

---
+++ day52.py
@@ -0,0 +1,6 @@
+#belajar ngoding def dengan parameter
+def identitas (nama,umur,alamat):
+	print ('nama = ', nama)
+	print ('umur = ', umur)
+	print ('alamat = ' , alamat)
+identitas (nama='Nurul Muflikha' , umur=19 , alamat='sabang subik')
