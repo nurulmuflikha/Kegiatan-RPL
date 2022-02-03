f1573e77945302e6bd36c7a7d24fc588a3165510
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-04 00:39:48 +0800

    Add files via upload

---
+++ Operator Day55.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a = 4;
+
+    $b = 8;
+
+    //penjumlahan
+    echo $a + $b . "\n";
+
+    //pengurangan
+    echo $a - $b . "\n";
+
+    //perkalian
+    echo $a * $b . "\n";
+
+    //pembagian
+    echo $a / $b . "\n";
+
+    //sisabagi
+    echo $a % $b . "\n";
