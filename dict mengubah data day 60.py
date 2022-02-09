55782b95d83b38dc5319d010b4ac3d78f3ac70a3
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-09 20:04:52 +0800

    Add files via upload

---
+++ dict mengubah data day 60.py
@@ -0,0 +1,9 @@
+mahasiswa = {
+  'nama': 'Nurul Muflikha',
+  'asal': 'Pambusuang'
+}
+
+# mengubah data
+print('Nama awal:', mahasiswa.get('nama'))
+mahasiswa['nama'] = 'Mikha NM'
+print('Setelah diubah:', mahasiswa.get('nama'))
