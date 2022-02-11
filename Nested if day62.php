86635cc3d62935548637ae39cf9159de232e314c
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-11 21:33:09 +0800

    Add files via upload

---
+++ Nested if day62.php
@@ -0,0 +1,21 @@
+<?php
+
+ $user = "nurulmuflikha";
+ $pass = "password";
+
+ // cek username ada atau tidak
+ if ($user == "nurulmuflikha")
+
+    // cek password
+    if ($pass == "password")
+        echo ("Anda berhasil masuk");
+    
+    else
+        echo ("Password Anda salah");
+    
+
+
+ else 
+    echo ("Username belum di daftarkan");
+    
+?>
