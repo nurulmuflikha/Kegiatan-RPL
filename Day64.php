418d4017654a5530e11ab02aaedc1bf683695a38
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-13 19:57:47 +0800

    Add files via upload

---
+++ Day64.php
@@ -0,0 +1,15 @@
+<?php
+
+$umur = 20;
+$menikah = false;
+
+if($umur > 20)
+    if($menikah)
+        echo "Remaja";
+    else 
+        echo ("Orang Tua");
+    
+else 
+    echo ("Maaf website ini hanya untuk yang sudah berumur 20+");
+
+?>
