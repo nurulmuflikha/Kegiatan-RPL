8ebe73e06749fdfe2391491a9c8c27a1b8763bf9
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-17 22:10:41 +0800

    Add files via upload

---
+++ Day68.php
@@ -0,0 +1,11 @@
+<?php
+$angka1 = 15;
+$angka2 = 5;
+
+echo $angka1 + $angka2 . "<br>";
+echo $angka1 - $angka2 . "<br>";
+echo $angka1 * $angka2 . "<br>";
+echo $angka1 / $angka2 . "<br>";
+echo $angka1 % $angka2;
+
+?>
