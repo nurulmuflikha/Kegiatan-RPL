9f153828cb7ea200c9ec128115f084811cf71cd0
nurulmuflikha <96329042+nurulmuflikha@users.noreply.github.com>
2022-02-18 22:45:44 +0800

    Add files via upload

---
+++ Array_lagi_Day69.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama[0]="Nurul Muflikha";
+$nim[1]="D0221527";
+$hobby[2]="Renang";
+$prodi[3]="Teknik Informatika";
+
+echo " dikenal dengan nama " . $nama[0];
+echo " \n nim " . $nim[1];
+echo "\n hobby " . $hobby[2];
+echo  " \n prodi ".$prodi[3];
+
+?>
